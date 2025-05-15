6e579492cdb22b2d08a757208999383a0d089926
HenriTEL <henri.hannetel@pm.me>
2025-05-15 09:36:04 +0200

    Add Markdown parsing example

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # gitblog
+
+A blog-aware static website generator.
+
+## Dev Quickstart
+
+```bash
+# Build
+cargo build
+
+# Build and run
+cargo run
+```
+
+```python
+def hello():
+	s = "world"
+	print(f"Hello {world}")
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs};
 
 use gix_protocol::handshake::Ref;
 use gix_protocol::ls_refs;
@@ -6,6 +6,7 @@ use gix_url::Url;
 use gix_transport::bstr::BStr;
 use gix_transport::client::http;
 use prodash::progress;
+use comrak::{markdown_to_html_with_plugins, Plugins, plugins, Options};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let url = BStr::new("https://github.com/GitoxideLabs/gitoxide");
@@ -27,12 +28,33 @@ fn main() -> Result<(), Box<dyn Error>> {
         false,
     )?;
 
-    for ref_ in refs {
+    for ref_ in refs.iter().take(10) {
         match ref_ {
             Ref::Direct { full_ref_name, .. } => println!("{}", full_ref_name),
             _ => (),
         };
     }
+    readme_to_html();
 
     Ok(())
+}
+
+
+fn readme_to_html() {
+    let file_path = "README.md";
+    let md_input = fs::read_to_string(file_path)
+        .expect("Read README.md file");
+    let mut options = Options::default();
+    options.extension.footnotes = true;
+    options.extension.strikethrough = true;
+    options.extension.table = true;
+    options.extension.tasklist = true;
+
+    let code_highlighter = plugins::syntect::SyntectAdapter::new(Some("base16-eighties.dark"));
+    let mut plugins = Plugins::default();
+    plugins.render.codefence_syntax_highlighter = Some(&code_highlighter);
+
+
+    let html_output = markdown_to_html_with_plugins(&md_input, &Options::default(), &plugins);
+    println!("{html_output}");
 }
